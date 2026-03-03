0bf6b9289764b265a21bac157ff6237b79d5eb38
AnuragEnnippissery <anuragramesh608@gmail.com>
2026-03-03 12:32:23 +0530

    login page structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/home/header'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>fenrir</h1>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/home/Login.jsx
@@ -0,0 +1,19 @@
+function Login(){
+    return(
+        <>
+            <main>
+                <section>
+                    <h3>Expert level CyberSecurity in hours not weeks</h3>
+                    <p>What's included</p>
+                    <p>Effortlessly spider and map targets to uncover hidden security flaws</p>
+                    <p>Deliver high-quality , validated findings in hours , not weeks.</p>
+                    <p>Generate professional , enterprise -grade security reports automatically.</p>
+                </section>
+                <section>
+                    <p>form</p>
+                </section>
+            </main>
+        </>
+    )
+}
+export default Login;
